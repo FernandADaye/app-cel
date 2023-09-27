b6038cc8a78b298277c90c0bfc8e3676beae493d
Fernanda López <fer.daye.2007@gmail.com>
2023-09-27 11:08:43 -0600

    diseño

--- celularesapp/src/components/card/Card.jsx
+++ celularesapp/src/components/card/Card.jsx
@@ -1,11 +1,11 @@
 import './Card.css'
-function Card(props) {
+function Card(props){
     return(
         <div className='card-style'>            
             <img className='imagenCel' src="https://m.media-amazon.com/images/I/51iJx7YWDOL.__AC_SY445_SX342_QL70_ML2_.jpg" alt="" />
-            <h2 className='text'>{props.name}</h2>
+            <h2 className='text'>{props.item.title}</h2>
             <p className='text'>dato</p>
-            <p className='text'> <strong className='text'> precio:$23,897.00</strong></p>
+            <p className='text'><strong className='text'>{props.item.price}</strong></p>
         </div>
     )
 }
