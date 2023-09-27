1341e0aee7866a59b4fbb6cd359787e77504b022
Fernanda López <fer.daye.2007@gmail.com>
2023-09-26 20:02:47 -0600

    inicio

---
+++ celularesapp/src/components/card/Card.jsx
@@ -0,0 +1,7 @@
+import './Card.css'
+function Card() {
+    return(
+        <h1 className="titulo">holaaa</h1>
+    )
+}
+export default Card
---
+++ celularesapp/src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Card from './components/card/Card'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Card/>
+    <Card/>
+    <Card/>
+    <Card/>
+<h1>hola2</h1>
+  </React.StrictMode>,
+)
