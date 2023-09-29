83c5b369496759be5e3aa965c9bbc028bbd224cf
Fernanda López <fer.daye.2007@gmail.com>
2023-09-28 18:26:29 -0600

    correccion

--- celularesapp/src/main.jsx
+++ celularesapp/src/main.jsx
@@ -3,14 +3,45 @@ import ReactDOM from 'react-dom/client'
 import Card from './components/card/Card'
 import './index.css'
 
+const telefonos= [
+  {
+    title: 'xiaomi',
+    price:'10,000',
+  },
+  {
+    title: 'iphone',
+    price:'30,000',
+  }, 
+  {
+    title: 'Galaxi',
+    price:'20,000',
+  },
+  {
+    title: 'Motorola',
+    price:'20,000',
+  },
+  {
+    title: 'Samsum',
+    price:'10,000',
+  }
+]
+
+telefonos.forEach(items=>{
+  console.log(items);
+})
+
 ReactDOM.createRoot(document.getElementById('root')).render(
+
   <React.StrictMode>
     <section className='celulares'>
-    <Card name="iphone 15"/>
-    <Card name="moto"/>
-    <Card name="SAMSUM"/>
-    <Card name="xiaomi"/>
+      {
+      telefonos.map((item, index)=>{
+        // eslint-disable-next-line react/jsx-key
+        return <Card item={item} key={index}/>
+      })
+      }
     </section>
 
   </React.StrictMode>,
 )
+
