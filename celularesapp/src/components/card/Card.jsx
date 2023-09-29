112ff9048bb5a24caddbb164f98d38e4ebd7974f
Fernanda López <fer.daye.2007@gmail.com>
2023-09-29 14:16:02 -0600

    like false/true

--- celularesapp/src/components/card/Card.jsx
+++ celularesapp/src/components/card/Card.jsx
@@ -4,7 +4,6 @@ import { FaRegHeart } from "react-icons/fa";
 import { FaHeart } from "react-icons/fa";
 
 
-
 function Card(props){
     const [counter, setCounter]=useState(0)
 function handlerConter () {
@@ -12,13 +11,18 @@ function handlerConter () {
     
 }
 
+var [like, setLike]= useState(false)
+function handlerLike(){
+    setLike(!like)
+}
     return(
         <div className='card-style'>            
             <img className='imagenCel' src="https://m.media-amazon.com/images/I/51iJx7YWDOL.__AC_SY445_SX342_QL70_ML2_.jpg" alt="" />
             <div className='titleIcon'>
-            <h2 className='textT'>{props.item.title}</h2>
-            <FaRegHeart className='heartIcon'></FaRegHeart>
-            <FaHeart className='heartIcon'></FaHeart>
+                <h2 className='textT'>{props.item.title}</h2>
+                <div className='icons' onClick={handlerLike} >
+                    {like ? <FaRegHeart className='heartIcon' ></FaRegHeart> :<FaHeart className='heartIcon'></FaHeart> }
+            </div>
             </div>
             <p className='text'>{props.item.mas}</p>
             <p className='text'><strong className='text'>Costo ${props.item.price}</strong></p>
@@ -28,3 +32,4 @@ function handlerConter () {
     )
 }
 export default Card
+
