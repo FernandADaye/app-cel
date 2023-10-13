27b1e300d8de03bb08b32f664887a90bb13a1494
Fernanda López <fer.daye.2007@gmail.com>
2023-10-13 10:16:01 -0600

    Outlet/ Link

--- celularesapp/src/components/Home/Home.jsx
+++ celularesapp/src/components/Home/Home.jsx
@@ -1,13 +1,14 @@
 // import React from 'react'
-import Producto from '../Producto/Producto'
+// import Producto from '../Producto/Producto'
 import Menu from '../menu/Menu'
+import { Outlet } from 'react-router-dom'
 
 
 export default function home() {
     return (
 <>
 <Menu/>
-<Producto/> 
+<Outlet/>
 </>
 )
 }
