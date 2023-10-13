9bcf35f584f2270d7d653858c672dd35f2ee9ec0
Fernanda López <fer.daye.2007@gmail.com>
2023-10-12 20:01:57 -0600

    edit

---
+++ celularesapp/src/components/Home/Home.jsx
@@ -0,0 +1,13 @@
+// import React from 'react'
+import Producto from '../Producto/Producto'
+import Menu from '../menu/Menu'
+
+
+export default function home() {
+    return (
+<>
+<Menu/>
+<Producto/> 
+</>
+)
+}
